9a38ac5c792b42966b2fd389280022548a582486
Abdul Hafidh <abdul_h20@mhs.unsyiah.ac.id>
2022-01-03 22:10:36 +0700

    MENAMBAHKAN SOURCE CODE PERULANGAN

---
+++ Perulangan dan Array/ch1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    
+    // Perulangan membuat * sebanyak 10 baris memakai perulangan for
+
+    for(int i = 0; i < 10; i++){
+        printf("*\n");
+    }
+
+}
---
+++ Perulangan dan Array/ch3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    // Mencetak bintang sebanyak 10 baris dengan memakai perulangan do while
+
+    int i = 0;
+    do
+    {
+        printf("*\n");
+        i++;
+    } while (i < 10);
+    
+}
