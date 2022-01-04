2779e014911deb3b28e9742645a226d29a969e0b
Muhammad Rizky Hidayah Akbar <93087447+Rizbar@users.noreply.github.com>
2022-01-04 21:47:17 +0700

    -_-

---
+++ Bahasa Python/Operator/P2.py
@@ -0,0 +1,27 @@
+# Operator pengisi nilai A
+a = 10
+print("nilai a :", int(a))
+
+# Operator untuk menjumlahkan nilai dari A
+a += 5
+print("nilai a :", int(a))
+
+# Operator untuk mengurangkan nilai dari A
+a -= 3
+print("nilai a :", int(a))
+
+# Operator untuk mengalikan nilai dari A
+a *= 2
+print("nilai a :", int(a))
+
+# Operator untuk membagikan nilai dari A
+a /= 6
+print("nilai a :", int(a))
+
+# Operator untuk memoduluskan nilai dari A
+a %= 5
+print("nilai a :", int(a))
+
+# Operator untuk mempangkatkan nilai dari A
+a **= 4
+print("nilai a :", int(a))
